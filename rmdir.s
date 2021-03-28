df2fb4b962085fbf904ad4ba7d62be223aab676a
SickleTM <78701216+SickleTM@users.noreply.github.com>
2021-03-28 21:24:05 +0200

    Add files via upload

---
+++ rmdir.s
@@ -0,0 +1,27 @@
+BITS 64
+
+global _main
+
+
+section .rodata
+
+	path db 'ez', 0
+
+	cbon db 'fait.', 0
+	cbon_len equ $-cbon
+
+
+section .text
+
+_main:
+	mov rax, 84
+	mov rdi, path
+	mov rsi, cbon
+	mov rdx, cbon_len
+	syscall
+	jmp _exit
+
+_exit:
+	mov rax, 0x3C
+	mov rdi, 0
+	syscall
